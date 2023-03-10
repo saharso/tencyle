2496b5b85fe21bcf29fd02c143fab06e43fb38ae
saharso@gmail.com <saharso@gmail.com>
2023-03-10 07:50:54 +0200

    EndPoints enum

---
+++ src/shared/entities/api/EndPoints.ts
@@ -0,0 +1,6 @@
+enum EndPoints {
+  libraryList = "https://mdxod4qjvwpiv3rpjcygjvlglu0grhra.lambda-url.us-east-1.on.aws/",
+  libraryVuln = "https://z44wwp4c7v7b65rcjpijeco2pm0jwmlv.lambda-url.us-east-1.on.aws/",
+}
+
+export default EndPoints;
