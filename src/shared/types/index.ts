ba325ba179660e43e74cbf74c0b3b502c04bea3b
saharso@gmail.com <saharso@gmail.com>
2023-03-11 09:57:24 +0200

    select box

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,3 +1,5 @@
 import _FetchVulnSearchParams from "./FetchVulnSearchParams";
+import _SelectOption from "./SelectOption";
 
 export type FetchVulnSearchParams = _FetchVulnSearchParams;
+export type SelectOption = _SelectOption;
