570301a64cd43fe59231b1a2aec24d97d9fcaa49
saharso@gmail.com <saharso@gmail.com>
2023-03-12 19:15:10 +0200

    VulnerabiltiesGrid component

--- src/app/entities/VulnerabilitiesGrid/VulnerabilitiesGrid.tsx
+++ src/app/entities/VulnerabilitiesGrid/VulnerabilitiesGrid.tsx
@@ -1,3 +1,57 @@
-export default function VulnerabilitiesGrid() {
-  return <div></div>;
+import DateCellType, { ColDef } from "../../../shared/components/Grid";
+import TGrid from "../../../shared/components/Grid/TGrid";
+import { VulnerabilitiesGridRow } from "../../../shared/types";
+import SeverityCellType from "./CellTypes/SeverityCellType/SeverityCellType";
+
+const columns: ColDef<Partial<VulnerabilitiesGridRow>>[] = [
+  {
+    field: "id",
+    hide: true,
+  },
+  {
+    field: "summary",
+    flex: 1,
+    headerName: "Summary",
+  },
+  {
+    field: "severity",
+    headerName: "Severity",
+    width: 100,
+    cellRenderer: SeverityCellType,
+  },
+  {
+    field: "published",
+    headerName: "Published",
+    minWidth: 80,
+    width: 110,
+    cellRenderer: DateCellType,
+  },
+  {
+    field: "modified",
+    headerName: "Modified",
+    minWidth: 80,
+    width: 110,
+    cellRenderer: DateCellType,
+  },
+];
+
+interface VulnerabilitiesGridProps {
+  data: VulnerabilitiesGridRow[];
+  loading: boolean;
+}
+
+export default function VulnerabilitiesGrid({
+  data,
+  loading,
+}: VulnerabilitiesGridProps) {
+  return (
+    <TGrid
+      columns={columns}
+      rows={data}
+      loading={loading}
+      onRowClick={(e) => {
+        console.log(e.api.getSelectedRows());
+      }}
+    />
+  );
 }
